99d1b87b0dfc40af8fe399f80e5da1ff7c0ed066
Oleksandr <arevkov107@gmail.com>
2026-08-07 19:57:55 +0300

    feat(b5): test navigation via link click

---
+++ tests/saucedemo-product-link.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test navigation via link click', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await page.getByRole('link', { name: 'Sauce Labs Backpack' }).first().click();
+    await expect(page.getByText('Sauce Labs Backpack')).toBeVisible();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory-item.html?id=4');
+
+}); 
